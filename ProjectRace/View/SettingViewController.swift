8cbb3a50966fd28cc3d65c3bc280176a581ad9eb
evgenTrifonov <jonn.trifonov@gmail.com>
2022-03-15 02:46:32 +0500

    adding settings

--- ProjectRace/View/SettingViewController.swift
+++ ProjectRace/View/SettingViewController.swift
@@ -2,21 +2,54 @@
 //  SettingViewController.swift
 //  ProjectRace
 //
-//  Created by Evgeny Trifonov on 24.02.2022.
+//  Created by Evgeny Trifonov on 15.03.2022.
 //
 
+import Foundation
 import UIKit
 
 class SettingViewController: UIViewController {
-
+    
+    var segmentController = UISegmentedControl()
+    var imageView = UIImageView()
+    
+    //let imagesFonView = UIImageView(image: UIImage(named: "oboi"))
+    var menuRace = ["RedCar", "BlueCar"]
+    let imageArrayCarRace = [UIImage(named: "car.png"),
+                             UIImage(named: "car2.png")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .blue
+        
+        
+        let navigationController = UINavigationController.init(rootViewController: ViewController())
+       
+       // view.backgroundColor = UIColor.gray
+        navigationItem.title = "Настройки"
+        navigationController.navigationBar.prefersLargeTitles = true
+      
+        //create Fon
+//        imagesFonView.contentMode = .scaleAspectFill
+//        view.addSubview(imagesFonView)
+//        imagesFonView.frame = CGRect(x: CGFloat.zero, y: CGFloat.zero, width: view.frame.width, height: view.frame.height)
+//        view.sendSubviewToBack(imagesFonView)
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: 130, height: 260)
+        //imageView.center = view.center
+        imageView.image = imageArrayCarRace[0]
+        view.addSubview(imageView)
+        
+        //create segment
+        segmentController = UISegmentedControl(items: self.menuRace)
+        segmentController.frame = CGRect(x: 100, y: 500, width: 200, height: 30)
+        view.addSubview(segmentController)
+        
         
         
     }
-
+    
 }
+
+
 
 
--- ProjectRace/View/ViewController.swift
+++ ProjectRace/View/ViewController.swift
@@ -9,32 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var playButtonOutlet: UIButton?
+    
+    @IBAction func playButton(_ sender: Any) {
+      
+    }
+    
+    @IBAction func settingButton(_ sender: Any) {
+    }
+    
     let imagesFonView = UIImageView(image: UIImage(named: "oboi"))
-    let playButton = UIButton()
-    let settingButton = UIButton()
+//    let playButton = UIButton()
+//    let settingButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //create button play
-        playButton.setTitle("Play", for: .normal)
-        playButton.backgroundColor = .red
-        playButton.titleLabel?.textColor = .white
-        playButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(playButton)
-        constraintButton()
-       
-        playButton.addTarget(self, action: #selector(goToViewButtonAction), for: .touchUpInside)
+       // let playButtonOutlet = UIButton()
+//        for button in playButtonOutlet {
+//            button.setTitle("Играть", for: .normal)
+//            playButton.backgroundColor = .red
+//            playButton.titleLabel?.textColor = .white
+//            playButton.translatesAutoresizingMaskIntoConstraints = false
+//            view.addSubview(playButton)
+//            constraintButton()
+    //    }
         
-        //create button setting
-        settingButton.setTitle("Setting", for: .normal)
-        settingButton.backgroundColor = .red
-        settingButton.titleLabel?.textColor = .white
-        settingButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(settingButton)
-        settingCnstraintButton()
+        
+        //create button play
        
-        settingButton.addTarget(self, action: #selector(goToSettingViewButtonAction), for: .touchUpInside)
+
+//        playButton.addTarget(self, action: #selector(goToViewButtonAction), for: .touchUpInside)
+//
+//        //create button setting
+//        settingButton.setTitle("Настройки", for: .normal)
+//        settingButton.backgroundColor = .red
+//        settingButton.titleLabel?.textColor = .white
+//        settingButton.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(settingButton)
+//        settingCnstraintButton()
+//
+//        settingButton.addTarget(self, action: #selector(goToSettingViewButtonAction), for: .touchUpInside)
 
         
         //create Fon
@@ -42,40 +57,44 @@ class ViewController: UIViewController {
         view.addSubview(imagesFonView)
         imagesFonView.frame = CGRect(x: CGFloat.zero, y: CGFloat.zero, width: view.frame.width, height: view.frame.height)
         view.sendSubviewToBack(imagesFonView)
-        
-    }
+//
+//    }
    
-    func constraintButton() {
-        playButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        playButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        playButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -50).isActive = true
-    }
+//    func constraintButton() {
+//        playButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        playButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+//        playButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -50).isActive = true
+//    }
+//
+//    func settingCnstraintButton() {
+//        settingButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        settingButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50).isActive = true
+//        settingButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -50).isActive = true
+//    }
     
-    func settingCnstraintButton() {
-        settingButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        settingButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 50).isActive = true
-        settingButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -50).isActive = true
-    }
+    
+  
     
     
-    @objc func goToViewButtonAction(_ sender: Any) {
-        let raceController = storyboard!.instantiateViewController(withIdentifier: "raceController")
-        raceController.modalPresentationStyle = .fullScreen
-        present(raceController, animated: true, completion: nil)
-
-       
-    }
-    
-    @objc func goToSettingViewButtonAction(_ sender: Any) {
-        let settingController = UIStoryboard(name: "SettingViewController", bundle: nil)
-        let oneControlller = settingController.instantiateViewController(withIdentifier: "settingController")
-       // present(, animated: true, completion: nil)
-        
-
+//    @objc func goToViewButtonAction(_ sender: Any) {
+//        let raceController = storyboard!.instantiateViewController(withIdentifier: "raceController")
+//        raceController.modalPresentationStyle = .fullScreen
+//        present(raceController, animated: true, completion: nil)
+//
+//
+//    }
+//
+//    @objc func goToSettingViewButtonAction(_ sender: Any) {
+//        let settingController = storyboard!.instantiateViewController(withIdentifier: "settingController")
+//        settingController.modalPresentationStyle = .fullScreen
+//        present(settingController, animated: true, completion: nil)
+//
+//
     }
  
   
 }
+
 
 
 
